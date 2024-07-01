27ed262887d2377a7da9208ff1c19325e77f28a5
Salvoo08 <165826507+Salvoo08@users.noreply.github.com>
2024-07-01 18:08:24 +0200

    beta1

--- main.py
+++ main.py
@@ -15,7 +15,24 @@ def index():
 @app.route('/', methods=['POST'])
 def process_form():
     button_python = request.form.get('button_python')
-    return render_template('index.html', button_python=button_python)
+    button_discord = request.form.get('button_discord')
+    button_html = request.form.get('button_html')
+    button_db = request.form.get('button_db')
+    return render_template(
+        'index.html', 
+        button_python=button_python,
+        button_discord=button_discord,
+        button_html=button_html,
+        button_db=button_db
+        )
+
+@app.route('/', methods=['POST'])
+def feebacks():
+    email = request.form.get('email')
+    text = request.form.get('text')
+    with open('feedbacks.txt', 'a') as f:
+        f.write(f'EMAIL: {email}; TEST: {text}')
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
